d55b94d4706e790abc5126caa492c2abfc645750
M-RAJAVIVEK <rajavivekmech@gmail.com>
2025-01-27 12:25:21 +0530

    updated content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import CreatePost from './CreatePost';
 import EditPost from './EditPost';
 import PostsList from './PostsList';
